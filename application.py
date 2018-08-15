ba56c18a27a0ea3386544658654a820355616eb2
tortorib <34718380+tortorib@users.noreply.github.com>
2018-08-15 14:31:53 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-from flask import (Flask, render_template, request, redirect, jsonify,
-                   url_for, flash)
+from flask import Flask, render_template, request, redirect, jsonify
+from flask import url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from application_db_setup import Base, Sport, RecentlyAdded, User
@@ -39,91 +39,6 @@ def showLogin():
     return render_template('login.html', STATE=state)
 
 
-@app.route('/fbconnect', methods=['POST'])
-def fbconnect():
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    access_token = request.data
-    print "access token received %s " % access_token
-    app_id = (json.loads(open('fb_client_secrets.json', 'r').read())
-              ['web']['app_id'])
-    app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.8/me"
-    '''
-        Due to the formatting for the result from the server token exchange we
-        have to split the token first on commas and select the first index
-        whichgives us the key : value for the server access token then we split
-        it on colons to pull out the actual token value and replace the
-        remaining quotes with nothing so that it can be used directly in the
-        graph api calls
-    '''
-    token = result.split(',')[0].split(':')[1].replace('"', '')
-
-    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
-    data = json.loads(result)
-    login_session['provider'] = 'facebook'
-    login_session['username'] = data["name"]
-    login_session['email'] = data["email"]
-    login_session['facebook_id'] = data["id"]
-
-    # The token must be stored in the login_session in order to properly logout
-    login_session['access_token'] = token
-
-    # Get user picture
-    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    data = json.loads(result)
-
-    login_session['picture'] = data["data"]["url"]
-
-    # see if user exists
-    user_id = getUserID(login_session['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-    login_session['user_id'] = user_id
-
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ''' " style = "width: 300px; height: 300px;border-radius:
-                    150px;-webkit-border-radius: 150px;-moz-border-radius:
-                    150px;"> '''
-
-    flash("Now logged in as %s" % login_session['username'])
-    return output
-
-
-@app.route('/fbdisconnect')
-def fbdisconnect():
-    facebook_id = login_session['facebook_id']
-    # The access token must me included to successfully logout
-    access_token = login_session['access_token']
-    url = '''https://graph.facebook.com/%s/permissions?access_token=%s'
-            % (facebook_id,access_token)'''
-    h = httplib2.Http()
-    result = h.request(url, 'DELETE')[1]
-
-    return "you have been logged out"
-
-
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     # Validate state token
@@ -141,8 +56,7 @@ def gconnect():
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
         response = make_response(
-                   json.dumps('Failed to upgrade the authorization code.'),
-                   401)
+            json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -177,13 +91,13 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps
-                                 ('Current user is already connected.'), 200)
+        response = make_response(json.dumps(
+          'Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Store the access token in the session for later use.
-    login_session['provider'] = 'google'
+    #login_session['provider'] = 'google'
     login_session['credentials'] = credentials
     login_session['gplus_id'] = gplus_id
     response = make_response(json.dumps('Successfully connected user', 200))
@@ -198,7 +112,7 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
     # ADD PROVIDER TO LOGIN SESSION
-    login_session['provider'] = 'google'
+    #login_session['provider'] = 'google'
 
     # see if user exists, if it doesn't make a new one
     user_id = getUserID(data["email"])
@@ -221,7 +135,6 @@ def gconnect():
 
 # User Helper Functions
 
-
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
@@ -237,35 +150,31 @@ def getUserInfo(user_id):
 
 
 def getUserID(email):
-    try:
-        user = session.query(User).filter_by(email=email).one()
-        return user.id
-    except:
-        return None
+    user = session.query(User).filter_by(email=email).one()
+    return user.id
+
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
-
 
 @app.route('/gdisconnect')
 def gdisconnect():
     # Only disconnect a connected user.
     access_token = login_session.get('access_token')
     if access_token is None:
-        response = (make_response(json.dumps('Current user not connected.'),
-                    401))
+        response = make_response(
+          json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
+    
     if result['status'] == '200':
         response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps
-                                 ('Failed to revoke token for given user.',
-                                  400))
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -297,7 +206,6 @@ def showSport():
         return render_template('sports.html', sport=sport)
 
 # Create a new Sport
-
 
 @app.route('/sport/new/', methods=['GET', 'POST'])
 def newSport():
@@ -382,8 +290,7 @@ def newRecentlyAdded(sport_id):
                                          user_id=sport.user_id)
         session.add(newRecentlyAdded)
         session.commit()
-        flash('New Recently Added %s Item Successfully Created'
-              % newRecentlyAdded.name)
+        flash('New Recently Added %s Item Successfully Created' % (newRecentlyAdded.name))
         return redirect(url_for('showSport', sport_id=sport_id))
     else:
         return render_template('newRecentlyAdded.html', sport_id=sport_id)
@@ -408,6 +315,7 @@ def editRecentlyAdded(sport_id, recently_added_id):
             editedItem.description = request.form['description']
         if request.form['price']:
             editedItem.price = request.form['price']
+            
         session.add(editedItem)
         session.commit()
         flash('Recently Added Item Successfully Edited')
@@ -438,29 +346,10 @@ def deleteRecentlyAdded(sport_id, recently_added_id):
 
 
 # Disconnect based on provider
-@app.route('/disconnect')
-def disconnect():
-    if 'provider' in login_session:
-        if login_session['provider'] == 'google':
-            gdisconnect()
-            del login_session['gplus_id']
-            # del login_session['access_token']
-        if login_session['provider'] == 'facebook':
-            fbdisconnect()
-            del login_session['facebook_id']
-        del login_session['username']
-        del login_session['email']
-        del login_session['picture']
-        del login_session['user_id']
-        del login_session['provider']
-        flash("You have successfully been logged out.")
-        return redirect(url_for('showSport'))
-    else:
-        flash("You were not logged in")
-        return redirect(url_for('showSport'))
+
 
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
